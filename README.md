6697687782e1f024ebce6ab5034f3b837245addb
ugm2 <unaigaraymaestre@gmail.com>
2022-12-26 12:26:11 +0100

    Adds Google Storage access tools
    Signed-off-by: ugm2 <unaigaraymaestre@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # cloud-storage
+
 Library to access Cloud Storage using Python
+
+## Installation
+
+Install Python in your machine and execute the following:
+
+```shell
+python -m venv env
+source ./env/bin/activate
+pip install -e .
+```
+
+## Run
+
+Authenticate with Google:
+
+```shell
+gcloud auth application-default login
+```
+
+Run example:
+
+```shell
+python cloud_storage/google.py
+```
---
+++ cloud_storage/google.py
@@ -0,0 +1,45 @@
+from google.cloud import storage
+
+STORAGE_CLASSES = ('STANDARD', 'NEARLINE', 'COLDLINE', 'ARCHIVE')
+
+class GCStorage:
+    def __init__(self, storage_client):
+        self.client = storage_client      
+
+    def get_bucket(self, bucket_name):
+        return self.client.bucket(bucket_name)
+
+    def upload_model(self, bucket, blob_destination, model_path):
+        model_name = model_path.split('/')[-1]
+        blob_destination += model_name
+        blob = bucket.blob(blob_destination)
+        blob.upload_from_filename(model_path)
+        return blob
+
+    def list_blobs(self, bucket, folder=None):
+        return bucket.list_blobs(prefix=folder)
+
+    def download_files_from_folder(self, bucket, folder, destination_folder):
+        blobs = self.list_blobs(bucket, folder)
+        for blob in blobs:
+            if not blob.name.endswith('/'):
+                # This blob is not a directory!
+                print(f'Downloading file [{blob.name}]')
+                filename = blob.name.split("/")[-1]
+                blob.download_to_filename(os.path.join(destination_folder, filename))
+
+if __name__ == "__main__":
+    
+    bucket_name = "dreambooth-bucket"
+    project_id = "bionic-repeater-368120"
+
+    # Construct GCStorage instance
+    storage_client = storage.Client(project=project_id)
+    gcs = GCStorage(storage_client)
+
+    # Cloud Storage bucket
+    bucket_gcs = gcs.get_bucket(bucket_name)
+    
+    # List files
+    blobs = gcs.list_blobs(bucket_gcs)
+    print([b for b in blobs])
